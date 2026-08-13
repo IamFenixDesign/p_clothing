c80565cb3f37e4d7e9cd334edef1e9ce95ec105a
Julian Gomez <julian.javier95@hotmail.com>
2026-08-12 21:45:49 -0300

    Release

---
+++ shared/config.lua
@@ -0,0 +1,14 @@
+return {
+    toggle = false,
+    defaultKey = "y",
+
+    commands = false,
+    allowincars = false,
+    allowragdolled = false,
+    allowinwater = false,
+
+    position = { x = 70, y = 50 }, -- min: 0, max: 100 (%)
+
+    -- Match illenium-appearance fenix-hud accent (#7c6cf0)
+    activeColor = "rgba(124, 108, 240, 0.95)",
+}
